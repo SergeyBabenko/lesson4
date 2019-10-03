b7f1f9a54e3e97e9f2be5e54355256dbeafc0ef3
Sergey Babenko <sergey.babenko.v@outlook.com>
2019-10-04 01:08:50 +0300

    Task 4.10 for Lesson 4. Class "HomeCat" was added.

---
+++ ru.tinkoff/Task4.10.kt
@@ -0,0 +1,34 @@
+import java.util.Scanner
+
+val scan = Scanner(System.`in`)
+
+fun makeCatVoice(e: Cat) {
+    e.voice()
+}
+
+open class Cat {
+    open fun voice() {
+        println("Meow meow meow!!!")
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val usualCat = Cat()        // Создаем обычного кота
+
+    val name = scan.next()      // Считываем имя
+    val homeCat = HomeCat(name) // Передаём в конструктор
+
+    val num = scan.nextInt()
+
+    repeat(num) {
+        makeCatVoice(usualCat)
+        makeCatVoice(homeCat)
+    }
+}
+
+class HomeCat(var name: String) : Cat() {
+    override fun voice() {
+        println("Meoooooow! Feed me, $name!")
+    }
+}
